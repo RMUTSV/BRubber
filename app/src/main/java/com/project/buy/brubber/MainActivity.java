3cc5b7574823ffe13be377569f5fd6603ff360f9
Sasiporn <Sasiporn.409@gmail.com>
2017-11-28 17:22:44 +0700

    logo

--- app/src/main/java/com/project/buy/brubber/MainActivity.java
+++ app/src/main/java/com/project/buy/brubber/MainActivity.java
@@ -1,16 +1,40 @@
 package com.project.buy.brubber;
 
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+import com.project.buy.brubber.fragment.MainFragment;
+
+public class MainActivity extends Activity {
+
+    private Button btnstart;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        matchView();
     }
+
+    private void matchView(){
+        btnstart = (Button) findViewById(R.id.btnstart);
+    }
+
+    public void Btnstart (View view){
+
+        Intent intent = new Intent(getApplicationContext(), NetworkInfo.DetailedState.class);
+        startActivity(intent);
+
+    }
+
+
+
 }   // Main Class
